6fb32a7846f4322aa9de9ae5502376fb7cf6bcb5
Spsiko <157087350+Spsiko@users.noreply.github.com>
2025-11-13 16:10:33 -0500

    Dashboard and listing

--- web/src/routes/AppRoutes.tsx
+++ web/src/routes/AppRoutes.tsx
@@ -18,10 +18,12 @@ import Settings from "../pages/admin/Settings";
 import AdminProfile from "../pages/admin/AdminProfile";
 
 // student
-import StudDashboard from "../pages/student/Listings";
+import StudDashboard from "../pages/student/StudDashboard";
 import Sell from "../pages/student/Sell";
 import Messages from "../pages/student/Messages";
 import Account from "../pages/student/Account";
+import Listings from "../pages/student/Listings"
+import ListingDetailPage from "../pages/student/ListingDetails";
 
 export default function AppRoutes() {
   return (
@@ -46,11 +48,12 @@ export default function AppRoutes() {
 
       {/* student */}
       <Route path="/student" element={<Navigate to="/Listings" replace />} />
-      <Route path="Listings" element={<StudDashboard />} />
+      <Route path="Listings" element={<Listings />} />
       <Route path="/student/dashboard" element={<StudDashboard />} />
       <Route path="Sell" element={<Sell />} />
       <Route path="Messages" element={<Messages />} />
       <Route path="Account" element={<Account />} />
+      <Route path="/listing/:id" element={<ListingDetailPage />} />
 
       {/* 404 */}
       <Route path="*" element={<div style={{ padding: 20 }}>Not Found</div>} />
