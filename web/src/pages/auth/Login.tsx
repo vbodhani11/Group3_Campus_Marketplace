11b4fe7d1d2d1710747c055657aa7ec48a1e006e
Spsiko <157087350+Spsiko@users.noreply.github.com>
2025-12-04 16:29:30 -0500

    morecahnges

--- web/src/pages/auth/Login.tsx
+++ web/src/pages/auth/Login.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../../lib/supabaseClient";
-import "../../style/Login.scss";
+import "../../style/login.scss";
 
 type Role = "student" | "admin";
 
--- web/src/pages/auth/Register.tsx
+++ web/src/pages/auth/Register.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../../lib/supabaseClient";
-import "../../style/Register.scss";
+import "../../style/register.scss";
 
 type Restrictions = {
   campusEmailRequired?: boolean;
